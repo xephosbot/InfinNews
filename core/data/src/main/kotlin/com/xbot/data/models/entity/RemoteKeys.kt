a48ab9601b165619987035a43906903d8945fe03
Javokhir Isaev <75476237+xephosbot@users.noreply.github.com>
2025-09-29 09:23:24 +0500

    Update RemoteKeys.kt

--- core/data/src/main/kotlin/com/xbot/data/models/entity/RemoteKeys.kt
+++ core/data/src/main/kotlin/com/xbot/data/models/entity/RemoteKeys.kt
@@ -1,11 +1,10 @@
 package com.xbot.data.models.entity
 
 import androidx.room.Entity
-import androidx.room.PrimaryKey
 
-@Entity(tableName = "remote_keys")
+@Entity(tableName = "remote_keys", primaryKeys = ["articleUrl", "category"])
 internal data class RemoteKeys(
-    @PrimaryKey val articleUrl: String,
+    val articleUrl: String,
     val prevKey: Int?,
     val nextKey: Int?,
     val category: String
